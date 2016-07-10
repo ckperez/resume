9e5d05fac2877d91ebdd6ef949adba90693aec15
Chris Perez <chris.k.perez@gmail.com>
2016-07-09 20:12:36 -0700

    added scss files and altered resume info

--- app/js/resume/controllers/resumeController.js
+++ app/js/resume/controllers/resumeController.js
@@ -9,8 +9,7 @@ function ResumeController(){
       title:'Ping Pong Thing',
       link: 'http...',
       description: 'Ladder...',
-      tech: 'Stuff',
-      role: '...',
+      contributions:[],
       dates: '...',
       tags: ['Angular', 'Node.js', 'NoSQL', 'Sass']
     },
@@ -18,8 +17,7 @@ function ResumeController(){
       title:'PlopJS',
       link: 'http://npmjs.com/package/plopjs',
       description: 'Command line interface for streamlining project setup with easy-to-use boilerplates and templates.',
-      tech: 'Built on Node.js using fs-extra for file system manipulation, NodeGit for Git/GitHub interfacing, and vorpal for the CLI framework.',
-      role: 'Managed project, built primary functionality, oversaw documentation and testing.',
+      contributions:['Built primary functionality with Node.js and various NPM packages for file system manipulation and Git/GitHub integration', 'Oversaw testing with Mocha framework and Chai assertion library', 'Directed documentation in GitHub-flavor markdown'],
       dates: 'June 2016',
       tags: ['Node.js', 'Project Management', 'TDD', 'Command Line', 'I/O']
     },
@@ -27,8 +25,7 @@ function ResumeController(){
       title:'RepFindr',
       link: 'http://rep-findr.firebaseapp.com',
       description: 'Single-page application for finding elected US officials and their contact information based on user address.',
-      tech: 'Built with MVC structure, jQuery, Sass, Handlebars.js for HTML templating, Page.js for client-side routing, Google APIs for public data, and Firebase for deployment and collection of user data.',
-      role: 'Managed project, planned API calls and data handling, integrated modals.',
+      contributions:['Archtected project according to MVC principles', 'Used jQuery and Sass for UX/UI', 'Implemented HTML templating with Handlebars.js and client-side routing with Page.js', 'Leveraged Google Maps and Civic Data APIs and managed data programmatically', 'Created Firebase persistence layer to collect user data'],
       dates: 'May 2016',
       tags: ['MVC', 'jQuery', 'HTML Templating', 'Project Management', 'AJAX', 'Google Maps', 'Sass']
     }
@@ -37,37 +34,35 @@ function ResumeController(){
   this.experience = [
     {
       company: 'Student Experts',
-      positions:[
-        {
-          titles: ['Account Manager, Program Manager'],
-          dates: '1/13 - 6/16',
-          responsibilities: [
-            'Developed and implemented new content marketing products and services, and negotiated contract terms',
-            'Conferred with clients about the direction and execution of new content marketing strategies',
-            'Managed campaigns and products through lifecycle - concept, development, distribution, reworking, renewal, etc',
-            'Oversaw creative direction and brand consistency for clients with local and nationwide presence',
-            'Managed a rotating team of copywriters and marketers, selecting personnel to suit projects',
-            'Reported directly to CEO, interviewed job applicants, conducted performance reviews, implemented improvement plans'
-          ]
-        },
-        {
-          titles: ['Senior Project Manager'],
-          dates: '1/13 - 6/16',
-          responsibilities: [
-            'Oversaw content marketing programs for up to 65 clients on a monthly basis',
-            'Developed organizational structure and internal career tracks for writers and editors',
-            'Collaborated on development of new products and services',
-            'Developed a style guide for content marketing in the automotive industry'
-          ]
-        },
-        {
-          titles: ['Copywriting Contractor, Head Staff Writer, Copyeditor'],
-          dates: '1/13 - 6/16',
-          responsibilities: [
-            'Copyedited 75,000 words of online marketing content per week',
-            'Provided mentoring and training for new writers and led employee development'
-          ]
-        }
+      titles: 'Account Manager, Program Manager',
+      dates: '1/13 - 6/16',
+      responsibilities: [
+        'Developed and implemented new content marketing products and services, and negotiated contract terms',
+        'Conferred with clients about the direction and execution of new content marketing strategies',
+        'Managed campaigns and products through lifecycle - concept, development, distribution, reworking, renewal, etc',
+        'Oversaw creative direction and brand consistency for clients with local and nationwide presence',
+        'Managed a rotating team of copywriters and marketers, selecting personnel to suit projects',
+        'Reported directly to CEO, interviewed job applicants, conducted performance reviews, implemented improvement plans'
+      ]
+    },
+    {
+      company: 'Student Experts',
+      titles: 'Senior Project Manager',
+      dates: '1/13 - 6/16',
+      responsibilities: [
+        'Oversaw content marketing programs for up to 65 clients on a monthly basis',
+        'Developed organizational structure and internal career tracks for writers and editors',
+        'Collaborated on development of new products and services',
+        'Developed a style guide for content marketing in the automotive industry'
+      ]
+    },
+    {
+      company: 'Student Experts',
+      titles: 'Copywriting Contractor, Head Staff Writer, Copyeditor',
+      dates: '1/13 - 6/16',
+      responsibilities: [
+        'Copyedited 75,000 words of online marketing content per week',
+        'Provided mentoring and training for new writers and led employee development'
       ]
     }
   ];
